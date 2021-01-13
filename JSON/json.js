b081b3368cd1007414e8e1cd036a85168c8f6e80
lea78 <lea.hamzi@edu.itescia.fr>
2021-01-13 23:53:23 +0100

    style pour about me with font

--- JSON/json.js
+++ JSON/json.js
@@ -2,29 +2,36 @@
 const head = document.querySelector("top-banner");
 const mainInfo = document.querySelector("info");
 
+
 //récupération donné json par Fetch API
 fetch('./us.json')
     .then (response => response.json())
     .then(function(data){
         console.log(data);
         var {groupe,member}= data;
-        var titre =`<h2>${groupe}</h2>`;
+        var titre =`
+        <h2>${groupe}</h2>`;
         document.querySelector("#top-banner").innerHTML= titre; 
         
         
         for(var i=0; i < member.length; i++){
-            var {name,age,couleur, passion}=member[i];
+            var {name,age,couleur,passions}=member[i];
+            for(var j=0;j<passions.length;j++){
+                var myPassion = `<li>  ${passions[j]} </li>`;
+                console.log(passions);
+            }
             var markup = `
             <section>
-                <h3>${name}</h3>
+                <h3 id="member">${name}</h3>
                 <p> âge: ${age} ans </p>
                 <p> couleur favorite: ${couleur} </p>
-               `;
-               
-            
+                
+            </section>`;
+            console.log(`${name}`);
             //thanks my queen for the + <3
             document.querySelector("#info").innerHTML += markup;
-        }
+            }
+        
        
     })
 
