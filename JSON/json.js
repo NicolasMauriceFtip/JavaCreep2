438210dcf6431b22ba5520820f9ad9f9398bf999
lea78 <lea.hamzi@edu.itescia.fr>
2021-01-13 20:28:16 +0100

    Json 1ere version sans la liste des passions

--- JSON/json.js
+++ JSON/json.js
@@ -2,15 +2,26 @@
 const head = document.querySelector("top-banner");
 const mainInfo = document.querySelector("info");
 
-
-fetch('./people.json')
+//récupération donné json par Fetch API
+fetch('./us.json')
     .then (response => response.json())
     .then(function(data){
-        console.log(data);        
-        for(var i=0; i < data.length; i++){
-            var {name, manga}=data[i];
-            var markup = `<div>${name}, ${manga}</div>`;
-            console.log(`${name},${manga}`);
+        console.log(data);
+        var {groupe,member}= data;
+        var titre =`<h2>${groupe}</h2>`;
+        document.querySelector("#top-banner").innerHTML= titre; 
+        
+        
+        for(var i=0; i < member.length; i++){
+            var {name,age,couleur, passion}=member[i];
+            var markup = `
+            <section>
+                <h3>${name}</h3>
+                <p> âge: ${age} ans </p>
+                <p> couleur favorite: ${couleur} </p>
+               `;
+               
+            
             //thanks my queen for the + <3
             document.querySelector("#info").innerHTML += markup;
         }
