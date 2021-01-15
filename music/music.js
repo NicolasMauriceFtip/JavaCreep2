e81f12efd470bc1d7eac2f54d8176dc7b90de982
jesusa <justinjhes@gmail.com>
2021-01-15 12:22:21 +0100

    comments music

--- music/music.js
+++ music/music.js
@@ -1,62 +1,65 @@
- const audioPlayer = document.querySelector('audio');
+const audioPlayer = document.querySelector('audio');
 
-   audioPlayer.addEventListener('play', () => {
-   
-   
-       const contexteAudio = new AudioContext();
-       const src = contexteAudio.createMediaElementSource(audioPlayer);
-       const analyseur = contexteAudio.createAnalyser();
-   
-       const canvas = document.getElementById('canvas');
-       canvas.width = window.innerWidth;
-       canvas.height = 400;
-       const ctx = canvas.getContext('2d');
-   
-       src.connect(analyseur);
-       analyseur.connect(contexteAudio.destination);
-   
-       analyseur.fftSize = 1024;
-   
-       const frequencesAudio = analyseur.frequencyBinCount;
-       console.log(frequencesAudio);
-   
-       const tableauFrequences = new Uint8Array(frequencesAudio);
-   
-       const WIDTH = canvas.width;
-       const HEIGHT = canvas.height;
-   
-       const largeurBarre = (WIDTH / tableauFrequences.length) + 2;
-       let hauteurBarre;
-       let x;
-   
-       function retourneBarres(){
-   
-           requestAnimationFrame(retourneBarres)
-   
-           x = 0;
-   
-           analyseur.getByteFrequencyData(tableauFrequences);
-   
-           ctx.fillStyle = "#000"; 
-           ctx.fillRect(0,0,WIDTH,HEIGHT);
-   
-           for(let i = 0; i < frequencesAudio; i++){
-   
-               hauteurBarre = tableauFrequences[i];
-   
-               let r = 250;
-               let g = 50;
-               let b = i;
-   
-               ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
-               ctx.fillRect(x, HEIGHT, largeurBarre, -hauteurBarre)
-   
-               x += largeurBarre + 1;
-   
-           }
-   
-   
-       }
-       retourneBarres();
-   
-   })
+// event for audio
+audioPlayer.addEventListener('play', () => {
+	//AudioContext represents an audio-processing graph built from audio
+	const contexteAudio = new AudioContext();
+	//creates the connection between audio and audio context
+	const src = contexteAudio.createMediaElementSource(audioPlayer);
+	// to be able to manipulate the frequencies of audio
+	const analyseur = contexteAudio.createAnalyser();
+
+	//animation for audio
+	const canvas = document.getElementById('canvas');
+	canvas.width = window.innerWidth;
+	canvas.height = 400;
+	// view as 2dimension
+	const ctx = canvas.getContext('2d');
+	//connect to analyser to manipulate frequencies
+	src.connect(analyseur);
+	//connect to the devices sound
+	analyseur.connect(contexteAudio.destination);
+	//frequencies of audio
+	analyseur.fftSize = 1024;
+	//value of frequencies on canvas which takes the half of frequencies
+	const frequencesAudio = analyseur.frequencyBinCount;
+	console.log(frequencesAudio);
+	// convert frequencies to an array to be able to manipulate each frequency
+	const tableauFrequences = new Uint8Array(frequencesAudio);
+
+	//  height/width of animation created
+	const WIDTH = canvas.width;
+	const HEIGHT = canvas.height;
+	// width of the animation on the canvas
+	const largeurBarre = WIDTH / tableauFrequences.length + 2;
+	let hauteurBarre;
+	let x;
+
+	//represents each bar of aniamtion
+	function retourneBarres() {
+		requestAnimationFrame(retourneBarres);
+
+		x = 0;
+
+		analyseur.getByteFrequencyData(tableauFrequences);
+		//background color of animation
+		ctx.fillStyle = '#000';
+		ctx.fillRect(0, 0, WIDTH, HEIGHT);
+
+		//colors for the frequencies
+		for (let i = 0; i < frequencesAudio; i++) {
+			hauteurBarre = tableauFrequences[i];
+
+			let r = 250;
+			let g = 50;
+			let b = i;
+
+			ctx.fillStyle = `rgb(${r}, ${g}, ${b})`;
+			//this fills the canvas
+			ctx.fillRect(x, HEIGHT, largeurBarre, -hauteurBarre);
+			//to have each bar side by side
+			x += largeurBarre + 1;
+		}
+	}
+	retourneBarres();
+});
